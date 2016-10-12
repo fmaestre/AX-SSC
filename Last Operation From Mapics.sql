5dbd5e838be13c196ba82aca0350c0876e3f0d9b
ANGIO\fmaestre <francio.maestre@gmail.com>
2016-10-12 09:24:24 -0700

    no message

--- Last Operation From Mapics.sql
+++ Last Operation From Mapics.sql
@@ -1,24 +1,24 @@
-SELECT [Order], CAST(LastOP AS NVARCHAR) LastOP
-FROM
-(
-	SELECT [Order],	MAX(CAST(Oper AS NUMERIC)) LastOP,
-		   (SELECT MAX(CAST(Oper AS NUMERIC)) 
-			 FROM [dbo].MAPICS AS i
-			 WHERE i.[Order] = A.[Order]
-		   ) [Max]
-	FROM [dbo].MAPICS A
-	WHERE Status = '40'	
-	GROUP BY [Order]
-) LAST_OP
+SELECT [Order], RIGHT(REPLICATE('0',4) + CAST(LastOP AS NVARCHAR),4) LastOP
+FROM	(
+			SELECT	[Order],	
+					MAX(CAST(Oper AS NUMERIC))			LastOP,
+					(	
+						SELECT MAX(CAST(Oper AS NUMERIC)) 
+						FROM [dbo].MAPICS AS i
+						WHERE i.[Order] = A.[Order]
+					)									[Max]
+			FROM [dbo].MAPICS A
+			WHERE status = '40'	
+			GROUP BY [Order]
+		) LAST_OP
 WHERE  LAST_OP.LastOP <> [Max]
 UNION
 SELECT [Order], '' LastOP
 FROM [dbo].MAPICS A
-WHERE Status = ALL (SELECT status 
-					FROM [dbo].MAPICS B
-					WHERE B.[Order] = A.[Order]
+WHERE status = ALL (
+						SELECT status 
+						FROM [dbo].MAPICS B
+						WHERE B.[Order] = A.[Order]
 				   )
-      AND Status <> '40'
+      AND status <> '40'
 GROUP BY [Order]
-
-
